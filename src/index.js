64b1cbe7c5f24fdbddb26807f65f2e116f583d23
IngMaicollAndresCalderonPardo <ing.maicollcalderonpardo@gmail.com>
2025-06-13 19:53:15 -0500

    Modificacion index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // ✅ IMPORTANTE
 
-// Añadimos Bootstrap a nuestro proyecto
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-// ¡Importante! Los estilos propios deben ir después del Bootstrap para que no los sobrescriba
 import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Crear el root con React 18/19
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter> {/* ✅ ENVOLVER APP */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
-// Medición de rendimiento (opcional)
 reportWebVitals();
